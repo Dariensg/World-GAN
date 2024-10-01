47ece31344f1ad59e92cb14dbcf599a6ecac18bd
HellionGames <dsgillespie02@yahoo.com>
2024-09-30 22:15:57 -0400

    Remove prints and fix dimensions of scaling tensors

--- run_training.sh
+++ run_training.sh
@@ -1,3 +1,3 @@
-python ./main.py --input_area_name "full-sandstone-dirt" --discriminator1_input_area_name "full-sandstone" --discriminator2_input_area_name "full-dirt" --niter 100 --alpha_layer_type "all-ones"
+python ./main.py --input_area_name "hill-sandstone-dirt" --discriminator1_input_area_name "hill-dirt" --discriminator2_input_area_name "hill-sandstone" --niter 500
 
-sleep 1200
+sleep 2400
