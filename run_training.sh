3050d40924c3a904d149c7fd7d943ac445f383c0
Dariensg <dsgillespie02@yahoo.com>
2024-10-12 11:47:41 -0400

    Include empty layers in reals

--- run_training.sh
+++ run_training.sh
@@ -1,3 +1,2 @@
-python ./main.py --input_area_name "full-sandstone-dirt" --discriminator1_input_area_name "full-sandstone" --discriminator2_input_area_name "full-dirt" --niter 100 --alpha_layer_type "all-ones"
-
-sleep 1200
+#python ./main.py --input_area_name "full-sandstone-dirt" --discriminator1_input_area_name "full-sandstone" --discriminator2_input_area_name "full-dirt" --niter 100 --alpha_layer_type "all-ones"
+python ./main.py --niter 100 --alpha_layer_type "all-ones"
